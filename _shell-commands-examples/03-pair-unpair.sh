9ab74c9fb1e809c279cabeeaaf80971b231214eb
Andrea Franz <andrea@gravityblast.com>
2019-10-08 23:20:27 +0900

    fix example shell file

--- _shell-commands-examples/03-pair-unpair.sh
+++ _shell-commands-examples/03-pair-unpair.sh
@@ -1,5 +1,5 @@
 keycard-select
-keycard-set-secrets 000000 123456789012 KeycardTest
+keycard-set-secrets 123456 123456789012 KeycardTest
 keycard-pair
 keycard-open-secure-channel
 keycard-verify-pin {{ session_pin }}
