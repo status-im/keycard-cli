ce4e7b2f6ad3567e0834338571570712427096ea
Andrea Franz <andrea@gravityblast.com>
2019-10-08 23:19:39 +0900

    fix example shell file

--- _shell-commands-examples/02-init.sh
+++ _shell-commands-examples/02-init.sh
@@ -2,5 +2,5 @@ keycard-select
 # setting pin, puk, pairing password
 keycard-set-secrets 123456 123456789012 KeycardTest
 # or generate random secrets
-# keycard-set-secrets 123456 123456789012 KeycardTest
+# keycard-set-secrets
 keycard-init
